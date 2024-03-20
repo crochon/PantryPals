ee52cecfd8464582337ba4f29dc18aeadb6df80b
crrochon <crrochon@mtu.edu>
2024-03-20 11:54:26 -0400

    Created Add Item Button
    Button has been created to allow for items to be added to the database, currently has not functionality.

--- app/src/main/java/com/example/pantrypals/ui/HomePantry.kt
+++ app/src/main/java/com/example/pantrypals/ui/HomePantry.kt
@@ -1,21 +1,34 @@
 package com.example.pantrypals.ui
 
-import androidx.activity.ComponentActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.navigation.NavController
 
 
+/*TODO Create popup Warning user if item is already in database */
 @Composable
 fun HomePantry(){
+    val DarkGreen = Color(0, 100, 0)
     Box(
         contentAlignment = Alignment.BottomCenter
     ){
-        Text(text = "Hello")
+        OutlinedButton(
+            onClick = { },
+            enabled = true,
+            colors = ButtonDefaults.buttonColors(DarkGreen)
+            ) {
+            Text(text = "Add Item",
+                color = Color.White)
+        }
     }
 }
 
