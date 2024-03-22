910f819f970745523049f575faf202bf65b98d2c
Nathan Brewer <ndbrewer@mtu.edu>
2024-03-22 11:43:50 -0400

    Minor Spelling mistake

--- app/src/main/java/com/example/pantrypals/PantryModel.kt
+++ app/src/main/java/com/example/pantrypals/PantryModel.kt
@@ -3,5 +3,5 @@ package com.example.pantrypals
 data class PantryModel(
         var itemName: String,
         var itemCount: Int,
-        var itemExperiation: Int,
+        var ItemExpiration: Int,
 )
